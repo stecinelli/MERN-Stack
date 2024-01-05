a0816b340b3a00445cc6ad4048813d2a5b853ae9
Stéphanie Cinelli <109476506+stecinelli@users.noreply.github.com>
2024-01-05 17:58:20 +0100

    server

---
+++ jobify/router/jobRouter.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+const router = Router();
+
+import {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+} from '../controllers/jobController.js';
+
+router.get('/', getAllJobs);
+
+router.route('/').get(getAllJobs).post(createJob);
+router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob);
+
+export default router;
---
+++ jobify/server.js
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import morgan from 'morgan';
+
+// Routers
+import jobRouter from './router/jobRouter.js';
+
+const app = express();
+const port = process.env.PORT || 5100;
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use('/api/v1/jobs', jobRouter);
+
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: 'something went wrong' });
+});
